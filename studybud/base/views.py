d07950fcb30ec5baed1b08c09a0e9c7e0020fced
Zarko Gavric <zarko.gavric@sportion.net>
2023-07-13 17:02:15 +0200

    Added dynamic room key and show specific room by it value

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -11,8 +11,14 @@ rooms = [
 
 def home(request):
     context = {'rooms': rooms}
-    return render(request, 'home.html', context)
+    return render(request, 'base/home.html', context)
 
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        print(i)
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
