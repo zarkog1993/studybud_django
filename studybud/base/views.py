d93acf00f0d82c06f8500f96eb2d1641ab75fbfd
Zarko Gavric <zarulja93@gmail.com>
2023-07-16 21:33:24 +0200

    Finished first database setup for Django project

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,24 +1,22 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
-rooms = [
-    {'id': 1, 'name':'Lets Learn python.1'},
-    {'id': 2, 'name':'Design with me'},
-    {'id': 3, 'name':'Frontend developers'},
-]
+# rooms = [
+#     {'id': 1, 'name': 'Lets Learn python.1'},
+#     {'id': 2, 'name': 'Design with me'},
+#     {'id': 3, 'name': 'Frontend developers'},
+# ]
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        print(i)
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
